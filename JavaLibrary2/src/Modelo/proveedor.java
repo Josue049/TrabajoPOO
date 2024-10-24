f0f12d0fd0ed6acb80e05f228f7027482e33c444
josue <josue@192.168.0.159>
2024-10-24 02:04:51 -0500

    Marco Antonio y yo terminamos de programar las clases

---
+++ JavaLibrary2/src/Modelo/proveedor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author josue
+ */
+public class proveedor extends empresa{
+    
+    public void enviarPedido(){
+        System.out.println("enviando...");
+    }
+}
